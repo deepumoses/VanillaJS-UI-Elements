73d2a1d43671a6c86e4316ffaaab277aca8b1c5e
deepumoses <sonicdeepak@gmail.com>
2020-06-21 15:33:22 +0530

    scrollTop fix

--- src/navigation/NavBar-Shrink-OnScroll/navbar.js
+++ src/navigation/NavBar-Shrink-OnScroll/navbar.js
@@ -5,7 +5,7 @@ document.body.onscroll = function () {
   console.log(document.documentElement.scrollTop);
   if (
     document.body.scrollTop > 130 ||
-    document.documentElement.scrollTop > 20
+    document.documentElement.scrollTop > 130
   ) {
     navigation.style.height = "70px";
     logo.style.fontSize = "2rem";
